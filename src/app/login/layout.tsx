d34c3d43a8cf4b3800f8ac5dff62fbb39fb86ac0
kushikushi5969 <yuya.kushida1221@gmail.com>
2024-05-04 05:23:54 +0900

    update: メタ情報の更新

--- src/app/login/layout.tsx
+++ src/app/login/layout.tsx
@@ -1,7 +1,15 @@
+import { Metadata } from 'next'
 import authOptions from '@app/api/auth/[...nextauth]/options'
 import { getServerSession } from 'next-auth/next'
 import { redirect } from 'next/navigation'
 import React from 'react'
+
+export async function generateMetadata() {
+  return {
+    title: 'ログイン画面',
+    description: 'ログイン画面',
+  }
+}
 
 export default async function LoginLayout({ children }: React.PropsWithChildren) {
   const data = await getData()
