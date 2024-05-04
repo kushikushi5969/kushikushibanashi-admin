1e18d87ff0565865cf1a474c05ab3f41b35c6c6b
kushikushi5969 <yuya.kushida1221@gmail.com>
2024-05-04 18:00:44 +0900

    fix: 余計な余白の削除

--- src/components/LogoutButton/LogoutButton.tsx
+++ src/components/LogoutButton/LogoutButton.tsx
@@ -3,15 +3,8 @@ import { Button } from '@mui/material'
 
 export default function LogoutButton() {
   return (
-    <div className='ml-4'>
-      <Button
-        className='ml-4 text-blue-500'
-        onClick={() => signOut({ callbackUrl: '/login' })}
-        variant='outlined'
-        color='error'
-      >
-        ログアウト
-      </Button>
-    </div>
+    <Button className='ml-4' onClick={() => signOut({ callbackUrl: '/login' })} variant='outlined' color='error'>
+      ログアウト
+    </Button>
   )
 }
