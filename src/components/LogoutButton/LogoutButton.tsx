2dbb2eba6e11452d4b803b188bf212066ffc069e
kushikushi5969 <yuya.kushida1221@gmail.com>
2024-05-05 00:41:04 +0900

    update: Refineのlogout関数を指定

--- src/components/LogoutButton/LogoutButton.tsx
+++ src/components/LogoutButton/LogoutButton.tsx
@@ -1,9 +1,11 @@
-import { signOut } from 'next-auth/react'
 import { Button } from '@mui/material'
+import { useLogout } from '@refinedev/core'
 
 export default function LogoutButton() {
+  const { mutate: logout } = useLogout()
+
   return (
-    <Button className='ml-4' onClick={() => signOut({ callbackUrl: '/login' })} variant='outlined' color='error'>
+    <Button className='ml-4' onClick={() => logout({})} variant='contained' color='error'>
       ログアウト
     </Button>
   )
