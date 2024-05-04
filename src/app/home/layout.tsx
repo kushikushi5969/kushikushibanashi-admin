29c7303e3a32d4cca81f7f6b9ba66c5fef65f0e3
kushikushi5969 <yuya.kushida1221@gmail.com>
2024-05-05 08:12:13 +0900

    add: ホーム画面の作成

---
+++ src/app/home/layout.tsx
@@ -0,0 +1,30 @@
+import authOptions from '@app/api/auth/[...nextauth]/options'
+import { ThemedLayout } from '@components/ThemedLayout/ThemedLayout'
+import { getServerSession } from 'next-auth/next'
+import { redirect } from 'next/navigation'
+import React from 'react'
+
+export async function generateMetadata() {
+  return {
+    title: 'ホーム',
+    description: 'ホーム画面',
+  }
+}
+
+export default async function Layout({ children }: React.PropsWithChildren) {
+  const data = await getData()
+
+  if (!data.session?.user) {
+    return redirect('/login')
+  }
+
+  return <ThemedLayout>{children}</ThemedLayout>
+}
+
+async function getData() {
+  const session = await getServerSession(authOptions)
+
+  return {
+    session,
+  }
+}
