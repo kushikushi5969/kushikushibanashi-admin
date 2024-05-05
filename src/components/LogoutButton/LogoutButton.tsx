914cc5f2c67ff82ad9734e0d2e660e0f631ff2ec
kushikushi5969 <yuya.kushida1221@gmail.com>
2024-05-05 09:34:13 +0900

    update: ログアウトボタンをアイコンに変更

--- src/components/LogoutButton/LogoutButton.tsx
+++ src/components/LogoutButton/LogoutButton.tsx
@@ -1,12 +1,13 @@
-import { Button } from '@mui/material'
+import { IconButton } from '@mui/material'
+import LogoutIcon from '@mui/icons-material/Logout'
 import { useLogout } from '@refinedev/core'
 
 export default function LogoutButton() {
   const { mutate: logout } = useLogout()
 
   return (
-    <Button className='ml-4' onClick={() => logout({})} variant='contained' color='error'>
-      ログアウト
-    </Button>
+    <IconButton className='ml-2' onClick={() => logout({})} color='inherit'>
+      <LogoutIcon />
+    </IconButton>
   )
 }
